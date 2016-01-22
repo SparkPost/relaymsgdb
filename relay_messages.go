e6d020941b7c772a0a98ed19bb07e91716278607
Dave Gray <git@doesntsuck.com>
2016-01-22 16:18:18 -0700

    parse only relay_message events

--- relay_messages.go
+++ relay_messages.go
@@ -4,8 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
+	re "regexp"
 
-	_ "github.com/SparkPost/gosparkpost/events"
+	"github.com/SparkPost/gosparkpost/events"
 	"github.com/SparkPost/httpdump/storage"
 )
 
@@ -18,14 +19,53 @@ type RelayMsgParser struct {
 // data about each message in the relay_messages table.
 func (p *RelayMsgParser) ProcessRequests(reqs []storage.Request) error {
 	log.Printf("ProcessRequests called with %d requests\n", len(reqs))
-	for _, req := range reqs {
+	for i, req := range reqs {
 		var events []*json.RawMessage
 		err := json.Unmarshal([]byte(req.Data), &events)
 		if err != nil {
-			log.Printf("ProcessRequets failed to parse JSON:\n%s\n", req.Data)
+			log.Printf("ProcessRequests failed to parse JSON:\n%s\n", req.Data)
 		} else {
-			log.Printf("ProcessRequests got %d events from %s\n", len(events), req.Data)
+			log.Printf("ProcessRequests found %d events from request %d\n", len(events), i)
+			for _, event := range events {
+				err := p.ProcessEvent(event)
+				if err != nil {
+					return err
+				}
+			}
 		}
+	}
+	return nil
+}
+
+var relayMsg *re.Regexp = re.MustCompile(`^\s*\{\s*"msys"\s*:\s*{\s*"relay_message"\s*:`)
+
+func (p *RelayMsgParser) ProcessEvent(j *json.RawMessage) error {
+	if j == nil {
+		return nil
+	}
+
+	idx := relayMsg.FindStringIndex(string(*j))
+	if len(idx) == 0 || idx[0] < 0 {
+		log.Printf("ProcessEvent ignored event: %s\n", string(*j))
+		return nil
+	}
+
+	var blob map[string]map[string]events.RelayMessage
+	err := json.Unmarshal([]byte(*j), &blob)
+	if err != nil {
+		log.Printf("ProcessEvent failed to parse JSON:\n%s\n", string(*j))
+	} else {
+		msys, ok := blob["msys"]
+		if !ok {
+			log.Printf("ProcessEvent ignored event with no \"msys\" key: %s\n", string(*j))
+			return nil
+		}
+		msg, ok := msys["relay_message"]
+		if !ok {
+			log.Printf("ProcessEvent ignored event with no \"relay_message\" key: %s\n", string(*j))
+			return nil
+		}
+		log.Printf("%s => %s (%s)\n", msg.From, msg.To, msg.WebhookID)
 	}
 	return nil
 }
