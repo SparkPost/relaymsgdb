3840b57b099745f60cb52816535f6fdc68372983
Rich Leland <rich.leland@messagesystems.com>
2016-01-26 07:44:38 -0500

    Update gopg dependency

--- Godeps/_workspace/src/github.com/SparkPost/gopg/gopg.go
+++ Godeps/_workspace/src/github.com/SparkPost/gopg/gopg.go
@@ -12,44 +12,52 @@ type Config struct {
 	User string
 	Pass string
 	Opts map[string]string
+	Url  string
 }
 
 var whitespace *re.Regexp = re.MustCompile(`\s`)
 
 func Connect(cfg *Config) (*sql.DB, error) {
-	opts := make([]string, 0, 3)
+	var dsn string
+	if cfg.Url != "" {
+		dsn = cfg.Url
 
-	if cfg.Db != "" {
-		if whitespace.MatchString(cfg.Db) {
-			return nil, fmt.Errorf("whitespace in database names is not supported")
+	} else {
+		opts := make([]string, 0, 3)
+
+		if cfg.Db != "" {
+			if whitespace.MatchString(cfg.Db) {
+				return nil, fmt.Errorf("whitespace in database names is not supported")
+			}
+			opts = append(opts, fmt.Sprintf("dbname=%s", cfg.Db))
 		}
-		opts = append(opts, fmt.Sprintf("dbname=%s", cfg.Db))
+
+		if cfg.User != "" {
+			if whitespace.MatchString(cfg.User) {
+				return nil, fmt.Errorf("whitespace in user names is not supported")
+			}
+			opts = append(opts, fmt.Sprintf("user=%s", cfg.User))
+		}
+
+		if cfg.Pass != "" {
+			if whitespace.MatchString(cfg.Pass) {
+				return nil, fmt.Errorf("whitespace in passwords is not supported")
+			}
+			opts = append(opts, fmt.Sprintf("password=%s", cfg.Pass))
+		}
+
+		for k, v := range cfg.Opts {
+			if whitespace.MatchString(k) {
+				return nil, fmt.Errorf("whitespace in option names is not supported: [%s]", k)
+			} else if whitespace.MatchString(v) {
+				return nil, fmt.Errorf("whitespace in option values is not supported: [%s] (%s)", v, k)
+			}
+			opts = append(opts, fmt.Sprintf("%s=%s", k, v))
+		}
+
+		dsn = strings.Join(opts, " ")
 	}
 
-	if cfg.User != "" {
-		if whitespace.MatchString(cfg.User) {
-			return nil, fmt.Errorf("whitespace in user names is not supported")
-		}
-		opts = append(opts, fmt.Sprintf("user=%s", cfg.User))
-	}
-
-	if cfg.Pass != "" {
-		if whitespace.MatchString(cfg.Pass) {
-			return nil, fmt.Errorf("whitespace in passwords is not supported")
-		}
-		opts = append(opts, fmt.Sprintf("password=%s", cfg.Pass))
-	}
-
-	for k, v := range cfg.Opts {
-		if whitespace.MatchString(k) {
-			return nil, fmt.Errorf("whitespace in option names is not supported: [%s]", k)
-		} else if whitespace.MatchString(v) {
-			return nil, fmt.Errorf("whitespace in option values is not supported: [%s] (%s)", v, k)
-		}
-		opts = append(opts, fmt.Sprintf("%s=%s", k, v))
-	}
-
-	dsn := strings.Join(opts, " ")
 	dbh, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
